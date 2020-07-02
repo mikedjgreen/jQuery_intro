9c2df1c7814090ef5274338455022e334cde406f
Mike Green <m.d.j.green@ntlworld.com>
2020-07-02 14:53:00 +0000

    xhr API

--- js/ajax-test.js
+++ js/ajax-test.js
@@ -1,14 +1,10 @@
-$(document).ready(function() {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         document.getElementById("data").innerHTML = this.responseText;
-    }
+    } 
 };
 
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 
 xhr.send();
-
-}); 
